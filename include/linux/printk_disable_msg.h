b0df00a1436913930429ee52447780637684fb37
darkhz <kmachanwenw@gmail.com>
2021-02-10 12:38:13 +0530

    include: linux: Add a header file to disable debug messages
    Certain drivers spam dmesg a lot, and individually editing their
    code to suppress printk messages can be a burden.Create this header
    file with empty printk defs, and strategically include this
    file in said drivers to prevent them from printing any messages.

---
+++ include/linux/printk_disable_msg.h
@@ -0,0 +1,27 @@
+#ifdef CONFIG_PRINTK_DISABLE_MSG
+
+#undef printk
+#undef pr_info
+#undef pr_err
+#undef pr_warn
+#undef pr_dbg
+#undef pr_debug
+
+#define printk(fmt, ...)
+#define pr_info(fmt, ...)
+#define pr_err(fmt, ...)
+#define pr_warn(fmt, ...)
+#define pr_dbg(fmt, ...)
+#define pr_debug(fmt, ...)
+
+#undef dev_info
+#undef dev_dbg
+#undef dev_err
+#undef dev_err_ratelimited
+
+#define dev_info(dev, fmt, ...)
+#define dev_dbg(dev, fmt, ...)
+#define dev_err(dev, fmt, ...)
+#define dev_err_ratelimited(dev, fmt, ...)
+
+#endif
